688fedc37bec2e4878a0b93c9e8d4360967c6149
dem08656775 <70008686+dem08656775@users.noreply.github.com>
2026-02-07 15:17:34 +0900

    Add files via upload

--- automation.js
+++ automation.js
@@ -1,66 +1,108 @@
 function Automationdata() {
-    this.configautobuyer = function (data, index) {
-        if (index == 0) {
-            let input = window.prompt("リセット時入手段位を設定", "")
-            input = new Decimal(input)
-            data.autolevelnumber = input
-        } else if (index == 1) {
-            let input = window.prompt("昇段停止段位を設定", "")
-            input = new Decimal(input)
-            data.autolevelstopnumber = input
-        } else if (index == 2) {
-            let input = window.prompt("リセット時入手階位を設定", "")
-            input = new Decimal(input)
-            data.autoranknumber = input
+  this.configautobuyer = function (data, index) {
+    if (index == 0) {
+      let input = window.prompt("リセット時入手段位を設定", "");
+      input = new Decimal(input);
+      data.autolevelnumber = input;
+    } else if (index == 1) {
+      let input = window.prompt("昇段停止段位を設定", "");
+      input = new Decimal(input);
+      data.autolevelstopnumber = input;
+    } else if (index == 2) {
+      let input = window.prompt("リセット時入手階位を設定", "");
+      input = new Decimal(input);
+      data.autoranknumber = input;
+    } else if (index == 3) {
+      let input = window.prompt(
+        "段位リセットポイント閾値を設定（0で無効）",
+        "",
+      );
+      input = new Decimal(input);
+      data.autolevelpoint = input;
+    } else if (index == 4) {
+      let input = window.prompt(
+        "階位リセットポイント閾値を設定（0で無効）",
+        "",
+      );
+      input = new Decimal(input);
+      data.autorankpoint = input;
+    }
+  };
+
+  this.toggleautobuyer = function (data, index) {
+    if (index == 0) data.genautobuy = !data.genautobuy;
+    if (index == 1) data.accautobuy = !data.accautobuy;
+    if (index == 2) data.autolevel = !data.autolevel;
+    if (index == 3) data.litemautobuy = !data.litemautobuy;
+    if (index == 5) data.autorank = !data.autorank;
+  };
+
+  this.updateAutoBuyers = function (data) {
+    let autorankshine = Math.max(0, 1000 - data.checkremembers() * 10);
+
+    if (
+      !data.player.onchallenge &&
+      data.player.rankchallengebonuses.includes(14) &&
+      data.autorank
+    ) {
+      if (
+        data.player.shine >= autorankshine &&
+        data.player.money.greaterThanOrEqualTo(
+          data.rankdata.resetRankborder(data),
+        )
+      ) {
+        let rankConditionMet = data.rankdata
+          .calcgainrank(data)
+          .greaterThanOrEqualTo(data.autoranknumber);
+        let rankPointConditionMet = data.autorankpoint.gt(0)
+          ? data.player.money.greaterThanOrEqualTo(data.autorankpoint)
+          : true;
+        if (rankConditionMet && rankPointConditionMet) {
+          data.resetRank(true);
+          data.player.shine -= autorankshine;
         }
+      }
     }
 
-    this.toggleautobuyer = function (data, index) {
-        if (index == 0) data.genautobuy = !data.genautobuy
-        if (index == 1) data.accautobuy = !data.accautobuy
-        if (index == 2) data.autolevel = !data.autolevel
-        if (index == 3) data.litemautobuy = !data.litemautobuy
-        if (index == 5) data.autorank = !data.autorank
+    if (data.player.rankchallengebonuses.includes(5) && data.litemautobuy) {
+      for (let i = 0; i < 5; i++) {
+        data.buylevelitems(i);
+      }
     }
 
-    this.updateAutoBuyers = function (data) {
-        let autorankshine = Math.max(0, 1000 - data.checkremembers() * 10)
-
-        if (!data.player.onchallenge && data.player.rankchallengebonuses.includes(14) && data.autorank) {
-            if (data.player.shine >= autorankshine && data.player.money.greaterThanOrEqualTo(data.rankdata.resetRankborder(data))) {
-                if (data.rankdata.calcgainrank(data).greaterThanOrEqualTo(data.autoranknumber)) {
-                    data.resetRank(true)
-                    data.player.shine -= autorankshine
-                }
-            }
+    if (
+      (data.player.rings.outsideauto.autodochallenge ||
+        !data.player.onchallenge) &&
+      data.activechallengebonuses.includes(14) &&
+      data.autolevel
+    ) {
+      if (
+        data.player.money.greaterThanOrEqualTo(data.resetLevelborder()) &&
+        data.player.level.lt(data.autolevelstopnumber)
+      ) {
+        let levelConditionMet = data
+          .calcgainlevel()
+          .greaterThanOrEqualTo(data.autolevelnumber);
+        let levelPointConditionMet = data.autolevelpoint.gt(0)
+          ? data.player.money.greaterThanOrEqualTo(data.autolevelpoint)
+          : true;
+        if (levelConditionMet && levelPointConditionMet) {
+          data.resetLevel(true, false);
         }
-
-        if (data.player.rankchallengebonuses.includes(5) && data.litemautobuy) {
-            for (let i = 0; i < 5; i++) {
-                data.buylevelitems(i)
-            }
-        }
-
-        if ((data.player.rings.outsideauto.autodochallenge || !data.player.onchallenge) && data.activechallengebonuses.includes(14) && data.autolevel) {
-            if (data.player.money.greaterThanOrEqualTo(data.resetLevelborder()) && data.player.level.lt(data.autolevelstopnumber)) {
-                if (data.calcgainlevel().greaterThanOrEqualTo(data.autolevelnumber)) {
-                    data.resetLevel(true, false)
-                }
-            }
-        }
-
-
-        if (data.activechallengebonuses.includes(5) && data.genautobuy) {
-            for (let i = 7; i >= 0; i--) {
-                data.buyGenerator(i)
-            }
-        }
-
-        if (data.activechallengebonuses.includes(9) && data.accautobuy) {
-            let ha = data.player.levelitems[3] + 1
-            for (let i = ha; i >= 0; i--) {
-                data.buyAccelerator(i)
-            }
-        }
+      }
     }
+
+    if (data.activechallengebonuses.includes(5) && data.genautobuy) {
+      for (let i = 7; i >= 0; i--) {
+        data.buyGenerator(i);
+      }
+    }
+
+    if (data.activechallengebonuses.includes(9) && data.accautobuy) {
+      let ha = data.player.levelitems[3] + 1;
+      for (let i = ha; i >= 0; i--) {
+        data.buyAccelerator(i);
+      }
+    }
+  };
 }
--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 Vue.createApp({
   data() {
     return {
@@ -30,6 +29,7 @@ Vue.createApp({
       leveldata: new Leveldata(),
       crowndata: new Crowndata(),
       worlddata: new Worlddata(),
+      markstonedata: new Markstonedata(),
 
       statuedata: new Statuedata(),
       storagedata: new Storagedata(),
@@ -44,6 +44,8 @@ Vue.createApp({
       autolevelnumber: new Decimal(2),
       autoranknumber: new Decimal(4),
       autolevelstopnumber: new Decimal("1e100"),
+      autolevelpoint: new Decimal(0),
+      autorankpoint: new Decimal(0),
       litemautobuy: false,
       autorank: false,
 
@@ -78,403 +80,413 @@ Vue.createApp({
 
       time: 0,
       diff: 0,
-    }
+    };
   },
   computed: {
     tweetLink() {
-      return this.socialdata.getTweetLink(this)
-    }
+      return this.socialdata.getTweetLink(this);
+    },
   },
   methods: {
     exportsave() {
-      this.storagedata.exportsave(this)
+      this.storagedata.exportsave(this);
     },
     exportsavefile() {
-      this.storagedata.exportsavefile(this)
+      this.storagedata.exportsavefile(this);
     },
     importsave() {
-      this.storagedata.importsave(this)
+      this.storagedata.importsave(this);
     },
     save() {
-      this.storagedata.save(this)
+      this.storagedata.save(this);
     },
     dataload() {
-      this.storagedata.dataload(this)
+      this.storagedata.dataload(this);
     },
     load(world) {
-      this.storagedata.load(this, world)
+      this.storagedata.load(this, world);
     },
     configshowmult() {
-      this.showmult = !this.showmult
+      this.showmult = !this.showmult;
     },
     softCap(num, cap) {
-      return this.utilsdata.softCap(num, cap)
+      return this.utilsdata.softCap(num, cap);
     },
     strongsoftcap(num, cap) {
-      return this.utilsdata.strongsoftcap(num, cap)
+      return this.utilsdata.strongsoftcap(num, cap);
     },
     calcgncost() {
-      this.generatordata.calcgncost(this)
+      this.generatordata.calcgncost(this);
     },
     calcaccost() {
-      this.acceleratordata.calcaccost(this)
+      this.acceleratordata.calcaccost(this);
     },
     calcdgcost() {
-      this.darkdata.calcdgcost(this)
+      this.darkdata.calcdgcost(this);
     },
     calclgcost() {
-      this.lightdata.calclgcost(this)
+      this.lightdata.calclgcost(this);
     },
     calccommonmult() {
-      this.generatordata.calccommonmult(this)
+      this.generatordata.calccommonmult(this);
     },
     calcincrementmult(i, to) {
-      return this.generatordata.calcincrementmult(this, i, to)
+      return this.generatordata.calcincrementmult(this, i, to);
     },
     calcbasicincrementmult(i) {
-      this.generatordata.calcbasicincrementmult(this, i)
+      this.generatordata.calcbasicincrementmult(this, i);
     },
     updategenerators(mu) {
-      this.generatordata.updategenerators(this, mu)
+      this.generatordata.updategenerators(this, mu);
     },
     updateaccelerators(mu) {
-      this.acceleratordata.updateaccelerators(this, mu)
+      this.acceleratordata.updateaccelerators(this, mu);
     },
     updatedarkgenerators(mu) {
-      this.darkdata.updatedarkgenerators(this, mu)
+      this.darkdata.updatedarkgenerators(this, mu);
     },
     updatelightgenerators(mu) {
-      this.lightdata.updatelightgenerators(this, mu)
+      this.lightdata.updatelightgenerators(this, mu);
     },
     spendshine(num) {
-      this.shinedata.spendshine(this, num)
+      this.shinedata.spendshine(this, num);
     },
     spendbrightness(num) {
-      this.shinedata.spendbrightness(this, num)
+      this.shinedata.spendbrightness(this, num);
     },
     spendflicker(num) {
-      this.shinedata.spendflicker(this, num)
+      this.shinedata.spendflicker(this, num);
     },
     buytype(num) {
-      this.shinedata.buytype(this, num)
+      this.shinedata.buytype(this, num);
     },
     calctoken() {
-      this.challengedata.calctoken(this)
+      this.challengedata.calctoken(this);
     },
     countpchallengecleared() {
-      this.challengedata.countpchallengecleared(this)
+      this.challengedata.countpchallengecleared(this);
     },
     findhighestgenerator() {
-      this.generatordata.findhighestgenerator(this)
+      this.generatordata.findhighestgenerator(this);
     },
     update() {
-      let diffm = this.timedata.updateTime(this)
+      let diffm = this.timedata.updateTime(this);
 
-      this.challengedata.updateActiveChallengeBonuses(this)
+      this.challengedata.updateActiveChallengeBonuses(this);
 
-      if (this.trophycheck) this.checktrophies()
-      this.checkmemories()
-      this.worlddata.checkworlds(this)
-      this.countsmalltrophies()
-      this.calccommonmult()
-      this.findhighestgenerator()
+      if (this.trophycheck) this.checktrophies();
+      this.checkmemories();
+      this.worlddata.checkworlds(this);
+      this.countsmalltrophies();
+      this.calccommonmult();
+      this.findhighestgenerator();
       for (let i = 0; i < 8; i++) {
-        this.calcbasicincrementmult(i)
+        this.calcbasicincrementmult(i);
       }
 
-      this.calcgncost()
-      this.calcaccost()
-      this.calcdgcost()
-      this.calclgcost()
+      this.calcgncost();
+      this.calcaccost();
+      this.calcdgcost();
+      this.calclgcost();
 
-      this.updategenerators(new Decimal(1))
-      this.updateaccelerators(new Decimal(1))
+      this.updategenerators(new Decimal(1));
+      this.updateaccelerators(new Decimal(1));
 
-      this.calctoken()
+      this.calctoken();
 
-      this.activateintimecampaign()
+      this.activateintimecampaign();
       if (this.calccampaigncosts() > this.player.accelevelused) {
-        alert("キャンペーン期間が終了しました。起動時間回帰力が不足しているため、時間回帰力の選択がリセットされます。")
-        this.player.activatedcampaigns = []
+        alert(
+          "キャンペーン期間が終了しました。起動時間回帰力が不足しているため、時間回帰力の選択がリセットされます。",
+        );
+        this.player.activatedcampaigns = [];
       }
 
-      this.shinedata.updateShine(this)
-      this.automationdata.updateAutoBuyers(this)
-      this.timedata.updateTickspeed(this)
+      this.shinedata.updateShine(this);
+      this.automationdata.updateAutoBuyers(this);
+      this.timedata.updateTickspeed(this);
 
-      setTimeout(this.update, Math.max(this.player.tickspeed - (this.diff + diffm) / 2, 1));
+      // 印石用tickカウンター更新（上限100万）
+      if (this.player.markstone.ticksSinceRankReset < 1000000) {
+        this.player.markstone.ticksSinceRankReset++;
+      }
+
+      setTimeout(
+        this.update,
+        Math.max(this.player.tickspeed - (this.diff + diffm) / 2, 1),
+      );
     },
     changeTab(tabname) {
       this.player.currenttab = tabname;
     },
     configtweet(content) {
-      this.socialdata.configtweet(this, content)
+      this.socialdata.configtweet(this, content);
     },
     configchallenge(index) {
-      this.challengedata.configchallenge(this, index)
+      this.challengedata.configchallenge(this, index);
     },
     configpchallenge(index) {
-      this.challengedata.configpchallenge(this, index)
+      this.challengedata.configpchallenge(this, index);
     },
     buyGenerator(index) {
-      this.generatordata.buyGenerator(this, index)
+      this.generatordata.buyGenerator(this, index);
     },
     buyAccelerator(index) {
-      this.acceleratordata.buyAccelerator(this, index)
+      this.acceleratordata.buyAccelerator(this, index);
     },
     buydarkgenerator(index) {
-      this.darkdata.buydarkgenerator(this, index)
+      this.darkdata.buydarkgenerator(this, index);
     },
     buylightgenerator(index) {
-      this.lightdata.buylightgenerator(this, index)
+      this.lightdata.buylightgenerator(this, index);
     },
     configautobuyer(index) {
-      this.automationdata.configautobuyer(this, index)
+      this.automationdata.configautobuyer(this, index);
     },
     toggleautobuyer(index) {
-      this.automationdata.toggleautobuyer(this, index)
+      this.automationdata.toggleautobuyer(this, index);
     },
     togglechipthresholduse() {
-      this.chipdata.togglechipthresholduse(this)
+      this.chipdata.togglechipthresholduse(this);
     },
     configchipthresholdnumber() {
-      this.chipdata.configchipthresholdnumber(this)
+      this.chipdata.configchipthresholdnumber(this);
     },
     autoshine() {
-      this.shinedata.autoshine(this)
+      this.shinedata.autoshine(this);
     },
     autobright() {
-      this.shinedata.autobright(this)
+      this.shinedata.autobright(this);
     },
     autochallenge() {
-      this.shinedata.autochallenge(this)
+      this.shinedata.autochallenge(this);
     },
     toggleringautobuyer(index) {
-      this.ringdata.toggleringautobuyer(this, index)
+      this.ringdata.toggleringautobuyer(this, index);
     },
     configringautobuyer(index) {
-      this.ringdata.configringautobuyer(this, index)
+      this.ringdata.configringautobuyer(this, index);
     },
     setbonusetype(index) {
-      this.challengedata.setbonusetype(this, index)
+      this.challengedata.setbonusetype(this, index);
     },
     setrankbonusetype(index) {
-      this.challengedata.setrankbonusetype(this, index)
+      this.challengedata.setrankbonusetype(this, index);
     },
     changebonusetype(index) {
-      this.challengedata.changebonusetype(this, index)
+      this.challengedata.changebonusetype(this, index);
     },
     changerankbonusetype(index) {
-      this.challengedata.changerankbonusetype(this, index)
+      this.challengedata.changerankbonusetype(this, index);
     },
     buyRewards(index) {
-      this.challengedata.buyRewards(this, index)
+      this.challengedata.buyRewards(this, index);
     },
     buyrankRewards(index) {
-      this.challengedata.buyrankRewards(this, index)
+      this.challengedata.buyrankRewards(this, index);
     },
     calclevelitemcost(index) {
-      return this.levelshopdata.calclevelitemcost(this, index)
+      return this.levelshopdata.calclevelitemcost(this, index);
     },
     buylevelitems(index) {
-      this.levelshopdata.buylevelitems(this, index)
+      this.levelshopdata.buylevelitems(this, index);
     },
     setmodetype() {
-      this.generatordata.setmodetype(this)
+      this.generatordata.setmodetype(this);
     },
     changemodetype() {
-      this.generatordata.changemodetype(this)
+      this.generatordata.changemodetype(this);
     },
     clearsetchip() {
-      this.chipdata.clearsetchip(this)
+      this.chipdata.clearsetchip(this);
     },
     setchiptype() {
-      this.chipdata.setchiptype(this)
+      this.chipdata.setchiptype(this);
     },
     changechiptype() {
-      this.chipdata.changechiptype(this)
+      this.chipdata.changechiptype(this);
     },
     changeMode(index) {
-      this.generatordata.changeMode(this, index)
+      this.generatordata.changeMode(this, index);
     },
     resetData(force) {
-      this.storagedata.resetData(this, force)
+      this.storagedata.resetData(this, force);
     },
     calcgainlevel() {
-      return this.leveldata.calcgainlevel(this)
+      return this.leveldata.calcgainlevel(this);
     },
     configspendchip(i) {
-      this.chipdata.configspendchip(this, i)
+      this.chipdata.configspendchip(this, i);
     },
     resetDarklevel() {
-      this.darkdata.resetDarklevel(this)
+      this.darkdata.resetDarklevel(this);
     },
     resetLevel(force, exit) {
-      this.leveldata.resetLevel(this, force, exit)
+      this.leveldata.resetLevel(this, force, exit);
     },
     resetLevelborder() {
-      return this.leveldata.resetLevelborder(this)
+      return this.leveldata.resetLevelborder(this);
     },
     resetRank(force) {
-      this.rankdata.resetRank(this, force)
+      this.rankdata.resetRank(this, force);
     },
     calcgaincrown() {
-      return this.crowndata.calcgaincrown(this)
+      return this.crowndata.calcgaincrown(this);
     },
     resetCrownborder() {
-      return this.crowndata.resetCrownborder(this)
+      return this.crowndata.resetCrownborder(this);
     },
     resetCrown(force) {
-      this.crowndata.resetCrown(this, force)
+      this.crowndata.resetCrown(this, force);
     },
     calcchallengeid() {
-      return this.challengedata.calcchallengeid(this)
+      return this.challengedata.calcchallengeid(this);
     },
     getchallengeid(arr) {
-      return this.challengedata.getchallengeid(arr)
+      return this.challengedata.getchallengeid(arr);
     },
     getpchallengeid(arr) {
-      return this.challengedata.getpchallengeid(arr)
+      return this.challengedata.getpchallengeid(arr);
     },
     configchallengeweightkind(i) {
-      this.challengedata.configchallengeweightkind(this, i)
+      this.challengedata.configchallengeweightkind(this, i);
     },
     configchallengeweightvalue(i) {
-      this.challengedata.configchallengeweightvalue(this, i)
+      this.challengedata.configchallengeweightvalue(this, i);
     },
     showunclearedchallenges() {
-      this.challengedata.showunclearedchallenges(this)
+      this.challengedata.showunclearedchallenges(this);
     },
     showunclearedrankchallenges() {
-      this.challengedata.showunclearedrankchallenges(this)
+      this.challengedata.showunclearedrankchallenges(this);
     },
     calcchallengesarray(challengeid) {
-      return this.challengedata.calcchallengesarray(challengeid)
+      return this.challengedata.calcchallengesarray(challengeid);
     },
     startChallenge() {
-      this.challengedata.startChallenge(this)
+      this.challengedata.startChallenge(this);
     },
     startpChallenge() {
-      this.challengedata.startpChallenge(this)
+      this.challengedata.startpChallenge(this);
     },
     exitChallenge() {
-      this.challengedata.exitChallenge(this)
+      this.challengedata.exitChallenge(this);
     },
     exitpChallenge() {
-      this.challengedata.exitpChallenge(this)
+      this.challengedata.exitpChallenge(this);
     },
     gettrophyname(i) {
-      return this.trophydata.gettrophyname(this, i)
+      return this.trophydata.gettrophyname(this, i);
     },
     moveworld(i) {
-      this.worlddata.moveworld(this, i)
+      this.worlddata.moveworld(this, i);
     },
     shrinkworld(i) {
-      this.worlddata.shrinkworld(this, i)
+      this.worlddata.shrinkworld(this, i);
     },
     calcmaxpipe() {
-      return this.worlddata.calcmaxpipe(this)
+      return this.worlddata.calcmaxpipe(this);
     },
     openpipe(i) {
-      this.worlddata.openpipe(this, i)
+      this.worlddata.openpipe(this, i);
     },
     confchecktrophies() {
-      this.trophydata.confchecktrophies(this)
+      this.trophydata.confchecktrophies(this);
     },
     checktrophies() {
-      this.trophydata.checktrophies(this)
+      this.trophydata.checktrophies(this);
     },
     chipset(i, j) {
-      this.chipdata.chipset(this, i, j)
+      this.chipdata.chipset(this, i, j);
     },
     checkusedchips() {
-      this.chipdata.checkusedchips(this)
+      this.chipdata.checkusedchips(this);
     },
     calcstatuecost(i) {
-      return this.statuedata.calcstatuecost(this, i)
+      return this.statuedata.calcstatuecost(this, i);
     },
     buildstatue(i) {
-      this.statuedata.buildstatue(this, i)
+      this.statuedata.buildstatue(this, i);
     },
     calcpolishcost(i) {
-      return this.statuedata.calcpolishcost(this, i)
+      return this.statuedata.calcpolishcost(this, i);
     },
     polishstatue(i) {
-      this.statuedata.polishstatue(this, i)
+      this.statuedata.polishstatue(this, i);
     },
     calcpolishcostbr(i) {
-      return this.statuedata.calcpolishcostbr(this, i)
+      return this.statuedata.calcpolishcostbr(this, i);
     },
     polishstatuebr(i) {
-      this.statuedata.polishstatuebr(this, i)
+      this.statuedata.polishstatuebr(this, i);
     },
     buyspirit(i) {
-      return
+      return;
       this.player.spiritlevela[i] += 1;
     },
     isavailablering(i) {
-      return this.ringdata.isavailablering(this, i)
+      return this.ringdata.isavailablering(this, i);
     },
     configsetrings(i) {
-      this.ringdata.configsetrings(this, i)
+      this.ringdata.configsetrings(this, i);
     },
     sleep(ms) {
-      this.utilsdata.sleep(ms)
+      this.utilsdata.sleep(ms);
     },
     configautomission() {
-      this.ringdata.configautomission(this)
+      this.ringdata.configautomission(this);
     },
     autoplaymission() {
-      this.ringdata.autoplaymission(this)
+      this.ringdata.autoplaymission(this);
     },
     isavailablemission(i) {
-      return this.ringdata.isavailablemission(this, i)
+      return this.ringdata.isavailablemission(this, i);
     },
     startmission(i) {
-      this.ringdata.startmission(this, i)
+      this.ringdata.startmission(this, i);
     },
     useskill(i) {
-      this.ringdata.useskill(this, i)
+      this.ringdata.useskill(this, i);
     },
     endmission() {
-      this.ringdata.endmission(this)
+      this.ringdata.endmission(this);
     },
     ringpointsum() {
-      return this.ringdata.ringpointsum(this)
+      return this.ringdata.ringpointsum(this);
     },
     worktime(val) {
-      this.timedata.worktime(this, val)
+      this.timedata.worktime(this, val);
     },
     calccampaigncosts() {
-      return this.timedata.calccampaigncosts(this)
+      return this.timedata.calccampaigncosts(this);
     },
     choosecampaigns(name) {
-      this.timedata.choosecampaigns(this, name)
+      this.timedata.choosecampaigns(this, name);
     },
     activateintimecampaign() {
-      this.timedata.activateintimecampaign(this)
+      this.timedata.activateintimecampaign(this);
     },
     counttrophies(index) {
-      this.trophydata.counttrophies(this, index)
+      this.trophydata.counttrophies(this, index);
     },
     checkpipedsmalltrophies() {
-      this.trophydata.checkpipedsmalltrophies(this)
+      this.trophydata.checkpipedsmalltrophies(this);
     },
     countsmalltrophies(index) {
-      this.trophydata.countsmalltrophies(this, index)
+      this.trophydata.countsmalltrophies(this, index);
     },
     checkmemories() {
-      this.rememberdata.checkmemories(this)
+      this.rememberdata.checkmemories(this);
     },
     checkremembers() {
-      return this.rememberdata.checkremembers(this)
+      return this.rememberdata.checkremembers(this);
     },
     checkworlds() {
-      this.worlddata.checkworlds(this)
+      this.worlddata.checkworlds(this);
     },
     toFormated(dec, exp) {
-      return this.utilsdata.toFormated(dec, exp)
-    }
+      return this.utilsdata.toFormated(dec, exp);
+    },
   },
   mounted() {
     this.dataload();
@@ -482,8 +494,8 @@ Vue.createApp({
     this.checkmemories();
     this.checkworlds();
     this.checkusedchips();
-    this.time = Date.now()
+    this.time = Date.now();
     setTimeout(this.update, this.player.tickspeed);
     setInterval(this.save, 2000);
   },
-}).mount('#app');
+}).mount("#app");
