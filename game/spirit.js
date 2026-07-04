34bddcc4fe4bd6ea9fe478a239c73c615ee76e03
dem08656775 <70008686+dem08656775@users.noreply.github.com>
2026-07-05 02:38:29 +0900

    Add files via upload

---
+++ game/spirit.js
@@ -0,0 +1,18 @@
+function Spiritdata() {
+
+	this.spiritnuma = 1;
+
+	this.spiritnamea = [
+		"鼠"
+	]
+
+	this.spiritcosta = [
+		new Decimal("1e180")
+	]
+
+	this.buyspirit = function (data, i) {
+		return
+		data.player.spiritlevela[i] += 1;
+	}
+
+}
